b91a44f7337879497ebb9d90d5d6d55a09b0fc12
isadauzaker <isadoravictor7@gmail.com>
2024-08-23 17:16:14 -0300

    Criação do primeiro projeto com React bonitão

---
+++ oiReact/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Dinossauros</h1>
+      <h2>Carnívoros</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus impedit soluta id cum, sit ea perferendis.
+         Sed, quibusdam ipsum! Dicta eaque, vitae quasi quo fugiat eum aspernatur quisquam molestiae dolorum.</p>
+    </>
+  )
+}
+
+export default App
