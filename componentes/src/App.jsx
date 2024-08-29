bc99b2cc98fc265b7d78b2c864786d5ca6187dc8
isadauzaker <isadoravictor7@gmail.com>
2024-08-29 17:24:59 -0300

    add componentes

---
+++ componentes/src/App.jsx
@@ -0,0 +1,39 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Atalhos do VS Code</h1>
+      <h2>Coisas legais para facilitar sua vida</h2>
+      <p>comando para o emmet da ul: ul>li*6</p>
+
+     <ul>
+      <li>Atalho: alt + z para quebra automática de linha</li>
+      <li>Atalho: alt + clique esquerdo para adicionar novos cursores na tela</li>
+      <li>Atalho: ctrl + shift + k para excluir uma linha inteira</li>
+      <li>Atalho: ctrl + ; para comentar/descomentar</li>
+      <li>Atalho: windows + . abre teclado de emojis</li>
+      <li>Atalho: alt + ↕ para mover uma linha inteira</li>
+      <li>Atalho: alt + shift + ↕ copia a linha atual ou bloco selecionado</li>
+      <li>Atalho: ctrl + j ou ctrl + ' abre/fecha o terminal</li>
+     </ul>
+     <p>Para esfregar vários cursores pela tela (digitar várias coisas ao mesmo tempo): Clica com a rodinha e esfrega ela pela tela.</p>
+
+     <p>Extensões:</p>
+     <ul>
+      <li>ES7 React/Redux/GraphQL/React-Native snippets</li>
+      <li>Auto Rename Tag</li>
+      <li>Color Highlight</li>
+      <li>Power Mode</li>
+      <li>vscode-pets</li>
+      <li>GlassIt-VSC
+</li>
+      
+     </ul>
+    </>
+  )
+}
+
+export default App
---
+++ componentesDeVerdade/src/components/Links.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Links() {
+  return (
+    <div>
+      <p>Link1 | link2 | link3 (tudo de !verdade)</p>
+    </div>
+  )
+}
+
+export default Links
