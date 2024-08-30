74e2bcde837343e84406982587ad4121195da63c
isadauzaker <isadoravictor7@gmail.com>
2024-08-30 17:10:44 -0300

    continuação do repositório

--- componentesDeVerdade/src/App.jsx
+++ componentesDeVerdade/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Cabecalho from './components/Cabecalho'
+import Corpo from './components/Corpo'
 import Links from './components/Links'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <>
       <Cabecalho />
       <Links />
+      <Corpo />
     </>
   )
 }
---
+++ componentesDeVerdade/src/components/Corpo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Corpo() {
+  return (
+    <div>
+      <h2>Eu sou o corpinho da página. Sou muito mais que um corpinho bonitinho...</h2>
+      <img src="./images/F%3Fria_da_Luz.webp" alt="" />
+    </div>
+  )
+}
+
+export default Corpo
