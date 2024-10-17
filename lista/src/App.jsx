3d789d82ea921fb741eb3fd694d06e90122705d7
ISADORA VICTOR DAUZAKER <isadora_dauzaker@estudante.sesisenai.org.br>
2024-10-17 16:45:25 -0300

    exercicios

---
+++ lista/src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+import Exemplo from './components/Exemplo'
+import Media from './components/Media'
+import CopoMeioCheio from './components/CopoMeioCheio'
+import DoisCoposMeioCheios from './components/DoisCoposMeioCheios'
+import StarUbers from './components/StarUbers'
+import Garcom from './components/Garcom'
+
+
+function App() {
+ 
+  return (
+    <>
+     <Exemplo />
+     <Garcom />
+     <Media />
+     <CopoMeioCheio />
+     <DoisCoposMeioCheios />
+     <StarUbers />
+     <Garcom />
+    </>
+  )
+}
+
+export default App
---
+++ lista/src/components/Exemplo.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react"
+
+function Exemplo() {
+
+    const [resultado, setResultado] = useState(0)
+
+    function aumentar(){
+        setResultado(resultado + 1)
+    }
+
+    function diminuir(){
+        setResultado(resultado - 1)
+    }
+
+  return (
+    <div>
+        <button onClick={aumentar}>+</button>
+      {resultado}
+      <button onClick={diminuir}>-</button>
+    </div>
+  )
+}
+
+export default Exemplo
