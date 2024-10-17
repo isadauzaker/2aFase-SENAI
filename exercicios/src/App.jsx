23e5c6d59aa7608168db56d12ea0e4a94df249cc
ISADORA VICTOR DAUZAKER <isadora_dauzaker@estudante.sesisenai.org.br>
2024-10-17 16:41:16 -0300

    exercicios

--- exercicios/src/App.jsx
+++ exercicios/src/App.jsx
@@ -1,23 +1,11 @@
+import { useState } from 'react'
 import './App.css'
-import Exemplo from './components/Exemplo'
-import Media from './components/Media'
-import CopoMeioCheio from './components/CopoMeioCheio'
-import DoisCoposMeioCheios from './components/DoisCoposMeioCheios'
-import StarUbers from './components/StarUbers'
-import Garcom from './components/Garcom'
-
 
 function App() {
- 
+
   return (
     <>
-     <Exemplo />
-     <Garcom />
-     <Media />
-     <CopoMeioCheio />
-     <DoisCoposMeioCheios />
-     <StarUbers />
-     <Garcom />
+
     </>
   )
 }
