87f6e92d978411d251d9897f197b3a2f7705b6f8
isadauzaker <isadoravictor7@gmail.com>
2024-11-07 17:17:27 -0300

    landing page

---
+++ landing page/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <nav className='container-navbar'>
+            <img src="/images/petmate.svg" alt="logo_PetMate" />
+
+            <div className='botoes-nav'>
+
+            </div>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
